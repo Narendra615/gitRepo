a0a4efd20d032c8355afd302257fae05fbbf8d1f
nkg <sonu1997.nk61@gmail.com>
2021-05-29 16:07:08 +0530

    chnages done

---
+++ App.java
@@ -0,0 +1,18 @@
+package com.mt.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+          System.out.println( "Hello World!" );
+       for(int i=0;i<10;i++){
+      System.out.println(i);
+
+         i=i-1;
+     i=i*2; 
+   }
+}
